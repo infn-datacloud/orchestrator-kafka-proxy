1e45537d51198f6deffb9ab3d6f861795f50ffc9
mperniola <michele.perniola@ba.infn.it>
2025-07-21 14:16:54 +0200

    fixed bytes parameter

--- app/ranking_service.py
+++ app/ranking_service.py
@@ -32,7 +32,9 @@ def root():
 
 @cpr_bp.route("/rank", methods=['POST'])
 def get_deployment_rank():
-    uuid = str(request.data)
+    uuid = request.data
+    if isinstance(uuid, bytes):
+        uuid = uuid.decode("utf-8")
     ranking_data = rp.get_ranking_data(uuid)
     if ranking_data:
         return ranking_data
